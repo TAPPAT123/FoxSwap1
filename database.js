4d4afe4e09f306a1ac17be3dfff5b8916c0f5919
TAPPAT123 <1tappat1@mail.ru>
2024-09-12 16:27:55 +0300

    Update database.js

--- database.js
+++ database.js
@@ -58,5 +58,5 @@
       "position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(255, 0, 0, 0.8); color: white; display: flex; justify-content: center; align-items: center; font-size: 24px; z-index: 1000;";
     document.body.appendChild(errorDiv);
   }
-  
+  // ... (ваш код для инициализации Firebase из firebase_config.js) ..
   
